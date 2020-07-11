27ff46f8fe388d46ae864b043ee3a2c1688dc067
Tonio_Cartonio <tonio@fantabos.co>
2020-07-11 15:24:50 +0200

    Added PacketMode and a basic BoostMode
    The boost mode is incomplete but better than nothing. PacketMode was added by Kami Blue so no point keeping it undisclosed.

--- src/main/java/com/matt/forgehax/mods/ElytraFlight.java
+++ src/main/java/com/matt/forgehax/mods/ElytraFlight.java
@@ -2,7 +2,11 @@ package com.matt.forgehax.mods;
 
 import static com.matt.forgehax.Helper.getLocalPlayer;
 import static com.matt.forgehax.Helper.getNetworkManager;
+import static com.matt.forgehax.Helper.getModManager;
 
+import com.matt.forgehax.asm.events.PacketEvent;
+
+import com.matt.forgehax.asm.events.LocalPlayerUpdateMovementEvent;
 import com.matt.forgehax.events.LocalPlayerUpdateEvent;
 import com.matt.forgehax.util.Switch.Handle;
 import com.matt.forgehax.util.command.Setting;
@@ -10,70 +14,230 @@ import com.matt.forgehax.util.entity.LocalPlayerUtils;
 import com.matt.forgehax.util.mod.Category;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketEntityAction.Action;
+import net.minecraft.network.play.server.SPacketEntityMetadata;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+
+/** Since KamiBlue added a packet mode I may
+ *    as well push mine too. The gist is public now anyway.
+ *  I'm also throwing in a super basic Boost mode, the good
+ *    one will remain undisclosed
+ */
 
 @RegisterMod
 public class ElytraFlight extends ToggleMod {
   
-  public final Setting<Boolean> fly_on_enable =
+  public enum FlyMode {
+    FLIGHT,
+    PACKET,
+    BOOST
+  }
+
+  public final Setting<Boolean> takeoff =
       getCommandStub()
           .builders()
           .<Boolean>newSettingBuilder()
-          .name("fly_on_enable")
-          .description("Start flying when enabled")
-          .defaultTo(false)
+          .name("takeoff")
+          .description("Send Elytra Open packet on enable")
+          .defaultTo(true)
           .build();
-  
+
   public final Setting<Double> speed =
       getCommandStub()
           .builders()
           .<Double>newSettingBuilder()
           .name("speed")
-          .description("Movement speed")
-          .defaultTo(0.05D)
+          .description("Flight speed in packet mode")
+          .defaultTo(0.0825D)
+          .build();
+
+  public final Setting<Float> boost =
+      getCommandStub()
+          .builders()
+          .<Float>newSettingBuilder()
+          .name("boost")
+          .description("Acceleration amount in boost mode")
+          .defaultTo(0.2F)
+          .build();
+
+  public final Setting<Double> maxboost =
+      getCommandStub()
+          .builders()
+          .<Double>newSettingBuilder()
+          .name("maxboost")
+          .description("Max speed in boost mode")
+          .defaultTo(1.75D)
+          .build();
+
+  public final Setting<Double> down_speed =
+      getCommandStub()
+          .builders()
+          .<Double>newSettingBuilder()
+          .name("down-speed")
+          .description("Downward speed when pressing Sneak")
+          .defaultTo(0.1D)
+          .build();
+  
+  public final Setting<Double> up_speed =
+      getCommandStub()
+          .builders()
+          .<Double>newSettingBuilder()
+          .name("up-speed")
+          .description("Upward speed when pressing Jump")
+          .defaultTo(0.1D)
+          .build();
+
+  public final Setting<FlyMode> mode =
+      getCommandStub()
+          .builders()
+          .<FlyMode>newSettingEnumBuilder()
+          .name("mode")
+          .description("The fly mode [flight/packet/boost]")
+          .defaultTo(FlyMode.PACKET)
           .build();
   
   private final Handle flying = LocalPlayerUtils.getFlySwitch().createHandle(getModName());
-  
+
   public ElytraFlight() {
-    super(Category.PLAYER, "ElytraFlight", false, "Elytra Flight");
+    super(Category.MOVEMENT, "ElytraFlight", false, "Elytra Flight");
+  }
+
+  @Override
+  public String getDisplayText() {
+    return (getModName() + " [" + mode.get() + "]");
   }
   
   @Override
   protected void onEnabled() {
-    if (fly_on_enable.get()) {
-      MC.addScheduledTask(
-          () -> {
-            if (getLocalPlayer() != null && !getLocalPlayer().isElytraFlying()) {
-              getNetworkManager()
-                  .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
-            }
-          });
+    if (getLocalPlayer() == null) return;
+    switch(mode.get()) {
+      case BOOST:
+      case FLIGHT:
+        if (takeoff.get()) {
+          MC.addScheduledTask(
+              () -> {
+                if (getLocalPlayer() != null && !getLocalPlayer().isElytraFlying()) {
+                  getNetworkManager()
+                      .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
+                }
+              });
+        }
+        break;
+      case PACKET:
+        MC.player.capabilities.isFlying = true;
+        MC.player.capabilities.allowFlying = true;
+        getLocalPlayer().capabilities.setFlySpeed(speed.getAsFloat());
+        break;
     }
   }
-  
+
   @Override
   public void onDisabled() {
-    flying.disable();
-    // Are we still here?
-    if (getLocalPlayer() != null) {
-      // Ensure the player starts flying again.
-      getNetworkManager()
-          .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
+    if (getLocalPlayer() == null) return;
+    switch(mode.get()) {
+      case FLIGHT:
+        flying.disable();
+        getNetworkManager()
+            .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
+        break;
+      case PACKET:
+        MC.player.capabilities.isFlying = false;
+        MC.player.capabilities.allowFlying = false;
+        getNetworkManager()
+            .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
+        break;
+      default: break;
+    }
+  }
+
+  @SubscribeEvent
+  public void onPacketInbound(PacketEvent.Incoming.Pre event) {
+    if (getLocalPlayer() == null) return;
+    switch(mode.get()) {
+      case PACKET:
+        if (event.getPacket() instanceof SPacketEntityMetadata) {
+          SPacketEntityMetadata MetadataPacket = event.getPacket();
+          if (MetadataPacket.getEntityId() == getLocalPlayer().getEntityId()) {
+            event.setCanceled(true);
+          }
+        }
+        break;
+      default: break;
     }
   }
   
   @SubscribeEvent
-  @SideOnly(Side.CLIENT)
   public void onLocalPlayerUpdate(LocalPlayerUpdateEvent event) {
-    // Enable our flight as soon as the player starts flying his elytra.
-    if (getLocalPlayer().isElytraFlying()) {
-      flying.enable();
+    if (getModManager().get(FreecamMod.class).get().isEnabled()) return;
+    if (getLocalPlayer() == null) return;
+    switch(mode.get()) {
+      case FLIGHT:
+        // Enable our flight as soon as the player starts flying his elytra.
+        if (getLocalPlayer().isElytraFlying()) {
+          flying.enable();
+        }
+        getLocalPlayer().capabilities.setFlySpeed(speed.getAsFloat());
+        break;
+      case PACKET:
+        getNetworkManager()
+            .sendPacket(new CPacketEntityAction(getLocalPlayer(), Action.START_FALL_FLYING));
+        MC.player.capabilities.isFlying = true;
+        MC.player.capabilities.allowFlying = true;
+        getLocalPlayer().capabilities.setFlySpeed(speed.getAsFloat());
+        break;
+      default: break;
     }
-    getLocalPlayer().capabilities.setFlySpeed(speed.getAsFloat());
+  }
+  @SubscribeEvent
+  public void onLocalPlayerUpdateMovement(LocalPlayerUpdateMovementEvent event) {
+    if (getLocalPlayer() == null) return;
+    if (getModManager().get(FreecamMod.class).get().isEnabled()) return;
+    switch(mode.get()) {
+      case BOOST:
+        if (!getLocalPlayer().isElytraFlying()) return;
+      
+        if (MC.gameSettings.keyBindSneak.isKeyDown()) {
+          getLocalPlayer().motionY = -down_speed.get();
+        } else if (MC.gameSettings.keyBindJump.isKeyDown()) {
+          getLocalPlayer().motionY = up_speed.get();
+        } else if (isPlayerIdle()) {
+          getLocalPlayer().setVelocity(0D, 0D, 0D);
+        } else {
+          getLocalPlayer().motionY = 0;
+        }
+      
+        float yaw = (float)Math.toRadians(getLocalPlayer().rotationYaw);
+        if (MC.gameSettings.keyBindForward.isKeyDown() || (getModManager().get("AutoWalk")).get().isEnabled()) {
+          getLocalPlayer().motionX -= MathHelper.sin(yaw) * boost.get();
+          getLocalPlayer().motionZ += MathHelper.cos(yaw) * boost.get();
+        } else if (MC.gameSettings.keyBindBack.isKeyDown()) {
+          getLocalPlayer().motionX += MathHelper.sin(yaw) * boost.get();
+          getLocalPlayer().motionZ -= MathHelper.cos(yaw) * boost.get();
+        }
+        if (MC.gameSettings.keyBindLeft.isKeyDown()) {
+          getLocalPlayer().motionX -= MathHelper.sin(yaw - (float) Math.toRadians(90.0F)) * boost.get();
+          getLocalPlayer().motionZ += MathHelper.cos(yaw - (float) Math.toRadians(90.0F)) * boost.get();
+        } else if (MC.gameSettings.keyBindRight.isKeyDown()) {
+          getLocalPlayer().motionX -= MathHelper.sin(yaw + (float) Math.toRadians(90.0F)) * boost.get();
+          getLocalPlayer().motionZ += MathHelper.cos(yaw + (float) Math.toRadians(90.0F)) * boost.get();
+        }
+        double speed = Math.sqrt(Math.pow(getLocalPlayer().motionZ, 2) + Math.pow(getLocalPlayer().motionX, 2));
+        if (speed > maxboost.get()) {
+          double factor = maxboost.get() / speed;
+          getLocalPlayer().motionX *= factor;
+          getLocalPlayer().motionZ *= factor;
+        } 
+        break;
+      default: break;
+    }
+  }
+  private static boolean isPlayerIdle() {
+    if (!MC.gameSettings.keyBindJump.isKeyDown() && !MC.gameSettings.keyBindSneak.isKeyDown() && 
+      !MC.gameSettings.keyBindForward.isKeyDown() && !MC.gameSettings.keyBindBack.isKeyDown() &&
+      !MC.gameSettings.keyBindLeft.isKeyDown() && !MC.gameSettings.keyBindRight.isKeyDown() &&
+      !(getModManager().get("AutoWalk")).get().isEnabled()) return true;
+    return false;
   }
 }
