68de325a564407107feea6681656ba70e3d19de9
Tonio_Cartonio <tonio@fantabos.co>
2020-07-25 05:53:39 +0200

    Added a macro keybind

--- src/main/java/com/matt/forgehax/mods/BaritoneCompatibility.java
+++ src/main/java/com/matt/forgehax/mods/BaritoneCompatibility.java
@@ -8,12 +8,38 @@ import com.matt.forgehax.util.command.Setting;
 import com.matt.forgehax.util.mod.Category;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
+
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 @RegisterMod
 public class BaritoneCompatibility extends ToggleMod {
+
+  private final KeyBinding bindMacro = new KeyBinding("Baritone Macro", Keyboard.KEY_K, "ForgeHax");
+
+  private final Setting<Boolean> auto_enable =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("start")
+          .description("Automatically enable when out of queue")
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Boolean> auto_stop =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("stop")
+          .description("Stop baritone when mod is disabled")
+          .defaultTo(true)
+          .build();
   
   private final Setting<String> on_string =
       getCommandStub()
@@ -32,9 +58,19 @@ public class BaritoneCompatibility extends ToggleMod {
           .description("Message to disable baritone")
           .defaultTo("#stop")
           .build();
+
+  private final Setting<String> macro_string =
+      getCommandStub()
+          .builders()
+          .<String>newSettingBuilder()
+          .name("macro-string")
+          .description("Command to execute on keybind press")
+          .defaultTo("#pause")
+          .build();
   
   public BaritoneCompatibility() {
     super(Category.MISC, "BaritoneCompatibility", false, "the lazy compatibility mod");
+    ClientRegistry.registerKeyBinding(this.bindMacro);
   }
   
   private boolean off = false;
@@ -53,16 +89,25 @@ public class BaritoneCompatibility extends ToggleMod {
   @Override
   protected void onDisabled() {
     off = once = false;
+    if (auto_stop.get())
+      turnOff();
   }
   
   @SubscribeEvent
   public void onWorldUnload(WorldEvent.Unload event) {
     onDisabled();
   }
+
+  @SubscribeEvent
+  public void onKeyPress(InputEvent.KeyInputEvent event) {
+    if (bindMacro.isPressed() && getLocalPlayer() != null) {
+      getLocalPlayer().sendChatMessage(macro_string.get());
+    }
+  }
   
   @SubscribeEvent
   public void onTick(LocalPlayerUpdateEvent event) {
-    if (!once) {
+    if (auto_enable.get() && !once) {
       once = true;
       BlockPos pos = getLocalPlayer().getPosition();
       if (pos.getX() != 0 && pos.getZ() != 0) {
